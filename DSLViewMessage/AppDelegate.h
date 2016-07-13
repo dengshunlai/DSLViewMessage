95abd3d3343ae938a6594ca6998371c1302742bf
dengshunlai <mu3305495@163.com>
2016-07-13 23:26:41 +0800

    Initial Commit

---
+++ DSLViewMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSLViewMessage
+//
+//  Created by 邓顺来 on 16/7/13.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DSLViewMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DSLViewMessage
+//
+//  Created by 邓顺来 on 16/7/13.
+//  Copyright © 2016年 邓顺来. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
